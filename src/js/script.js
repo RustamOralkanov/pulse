2c607bcf142d4b8821d53034bee601b7c63376cc
Rustam <oralkanov1r@icloud.com>
2022-05-19 16:28:11 +0600

    add carousel adaptive 767px and less

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick();
+    $('.carousel__inner').slick({
+      speed: 900,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/chevron-left.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/icon/chevron-right.svg" alt=""></button>',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            dots: true,
+            arrows: false
+          }
+        }
+      ]
+    });
   });
   
