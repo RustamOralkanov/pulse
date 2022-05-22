e6deacf49ab45f5dff6806d0d9b2f5ab8758ffbe
Rustam <oralkanov1r@icloud.com>
2022-05-22 20:53:38 +0600

    added footer with google map

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
       nextArrow: '<button type="button" class="slick-next"><img src="img/icon/chevron-right.svg" alt=""></button>',
       responsive: [
         {
-          breakpoint: 768,
+          breakpoint: 991,
           settings: {
             dots: true,
             arrows: false
