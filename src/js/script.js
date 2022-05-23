8f53f6f8acc6f33833f3614a928df7c430b95d13
Rustam <oralkanov1r@icloud.com>
2022-05-24 00:58:04 +0600

    add second level validation for forms

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,9 @@ $(document).ready(function(){
 					breakpoint: 991,
 					settings: {
 						dots: true,
-						arrows: false
+						arrows: false,
+						autoplay: true,
+  						autoplaySpeed: 2000
 					}
 				}
 			]
@@ -49,6 +51,40 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
+
+	//validation
+	function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
 
 
